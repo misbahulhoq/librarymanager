9e95781b7a4d9045594c88b73f1c253aa3907243
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-07-02 15:16:10 +0600

    removed unused imports

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,5 +1,5 @@
 import type { AppDispatch, RootState } from "@/redux/store";
-import { useDispatch, useSelector, useStore } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
